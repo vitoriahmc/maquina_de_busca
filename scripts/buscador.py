744486447ebc37d12fea82cfe34b188dee38bbda
Vitoria <vitoriahmc@insper.edu.br>
2020-04-01 17:06:15 -0300

    Fases 3 e 4 (Ranking e Distancia

--- scripts/buscador.py
+++ scripts/buscador.py
@@ -2,10 +2,23 @@ import json
 from argparse import ArgumentParser
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sexpr_tokenize
-
+from nltk.metrics.distance import edit_distance
 import search_engine.repository as se
 
+def distance_match(word, vocab):
+    m = 10e5
+    w = ''
+    for word2 in vocab:
+        dist = edit_distance(word, word2) 
+        if dist == 0:
+            w = word
+            break
+        if dist < m:
+            m = dist
+            w = word2
+    return f"Você quis dizer {w} ?"
 
+    
 def busca_and(index, query):
     query_terms = query.strip().split()
     if len(query_terms) == 0:
@@ -13,43 +26,54 @@ def busca_and(index, query):
 
     initial_term = query_terms[0]
     docids = set(index[initial_term]) if initial_term in index else set()
+    words = []
     ps = PorterStemmer()
     for word in query_terms[1:]:
         word = ps.stem(word)
         result = set(index[word]) if word in index else set()
+        words.append(word)
         docids &= result
 
-    return docids
+    return docids, words
 
 def busca_docids(index, query):
     result = [q.strip().strip('()') for q in sexpr_tokenize(query)]
 
     docids = set()
     for subquery in result:
-        res = busca_and(index, subquery)
+        res, words = busca_and(index, subquery)
         docids |= res
 
-    return docids
+    return docids, words
 
 def busca(corpus, repo, index, query):
     # Parsing da query.
     # Recuperar os ids de documento que contem todos os termos da query.
-    docids = busca_docids(index, query)
-
+    docids, words = busca_docids(index, query)
+    
     # Retornar os textos destes documentos.
-    return docids
+    return docids, words
 
 
-def ranking(corpus, repo, index, docids):
-    # Ranquear os documentos.
+def ranking(ranking_doc, docids, words):
+    ranked_docs = []
+    for doc_id in docids:
+        total = 0
+        for word in words:
+            tf = ranking_doc[doc_id][word]
+            idf = ranking_doc['idf'][word]
+            total += tf * idf
+        ranked_docs.append([doc_id, total])
 
-    return list(docids)  # dummy por enquanto.
+    ranked_docs = sorted(ranked_docs, key= lambda x: x[1])
+    return [doc_id for doc_id, _ in ranked_docs]
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('corpus', help='Arquivo do corpus')
     parser.add_argument('repo', help='Arquivo do repo.')
     parser.add_argument('index', help='Arquivo do index.')
+    parser.add_argument('ranking_file', help='Arquivo do index.')
     parser.add_argument('num_docs',
                         help='Numero maximo de documentos a retornar',
                         type=int)
@@ -63,9 +87,12 @@ def main():
 
     with open(args.index, 'r') as file:
         index = json.load(file)
+    
+    with open(args.ranking_file, 'r') as file:
+        ranking_doc = json.load(file)
 
-    docids = busca(corpus, repo, index, args.query)
-    docids_ranqueados = ranking(corpus, repo, index, docids)
+    docids, words = busca(corpus, repo, index, args.query)
+    docids_ranqueados = ranking(ranking_doc, docids, words)
     docs = [corpus[docid] for docid in docids_ranqueados[:args.num_docs]]
 
     print(docs)
--- scripts/indexador.py
+++ scripts/indexador.py
@@ -3,8 +3,8 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from argparse import ArgumentParser
-from collections import defaultdict
-
+from collections import defaultdict, Counter
+import math
 def create_repo(corpus):
     '''Cria o repositorio.
 
@@ -46,12 +46,41 @@ def create_index(repo):
         indexed[key] = list(indexed[key])
     return indexed
 
+def ranking(corpus):
+    ranking = {}
+    #tf
+    ps = PorterStemmer()
+    stop_words = set(stopwords.words('english'))
+        
+    ranking['total'] = {}
+    for doc_id, doc in corpus.items():
+        tokens = nltk.word_tokenize(doc)
+        stemmed = [ps.stem(x) for x in tokens if not x in stop_words] 
+        clean = [w for w in stemmed if w.isalpha()]
+        counter = Counter(clean)
+        ranking[doc_id] = {}
+        for key, value in counter.items():
+            ranking[doc_id][key] = (1+math.log(value,2))
+            try:
+                ranking['total'][key] += value
+            except KeyError:
+                ranking['total'][key] = value
+    #idf
+    ranking['idf'] = {}
+    for key, value in ranking['total'].items():
+        idf = math.log(len(corpus)/value, 2)
+        ranking['idf'][key] = idf
+
+    return ranking
+
 def main():
     parser = ArgumentParser()
     parser.add_argument('corpus',
                         help='Arquivo json com um dicionario docid para texto')
     parser.add_argument('repo_name',
                         help='Raiz do nome do arquivo de repositorio')
+    parser.add_argument('rank',
+                        help='Arquivo json com os tfs e idfs')
     args = parser.parse_args()
 
     with open(args.corpus, 'r') as file_corpus:
@@ -59,12 +88,16 @@ def main():
 
     repo = create_repo(corpus)
     index = create_index(repo)
+    rank = ranking(corpus)
 
     with open(args.repo_name + '_repo.json', 'w') as file_repo:
         json.dump(repo, file_repo, indent=4)
 
     with open(args.repo_name + '_index.json', 'w') as file_index:
         json.dump(index, file_index, indent=4)
+
+    with open(args.rank + '.json', 'w') as file_ranking:
+        json.dump(rank, file_ranking, indent=4)
 
 if __name__ == '__main__':
     main()
